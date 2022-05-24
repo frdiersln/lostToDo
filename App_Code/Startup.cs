e9808259cf5897f36970ffc1be0904975264ab3a
Ferdi Eraslan <121620191048@ogrenci.ogu.edu.tr>
2022-05-24 22:25:03 +0300

    Add project files.

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lostToDo.Startup))]
+namespace lostToDo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
