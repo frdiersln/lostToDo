88558f3a995ec5541db18e42440ff3cde1dd19ab
Ferdi Eraslan <121620191048@ogrenci.ogu.edu.tr>
2022-05-29 07:24:12 +0300

    to-do page finished

--- To-Do.aspx
+++ To-Do.aspx
@@ -1,7 +1,10 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="To-Do.aspx.cs" Inherits="Default2" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
-    <div class="top-bar"><%: Class %> | <%: Nick %></div>
+    <div class="top-bar">
+        <%: Class %> | <%: Nick %>
+        <asp:Button ID="BtnSave" CssClass="hiddenBtn" Text="Save" runat="server" AutoPostBack="true" OnClick="BtnSave_Click" />
+    </div>
     <hr />
     <div class="formContainer">
         <!-- 
@@ -14,42 +17,48 @@
         -->
         <div id="row1" class="rows"> 
             <div class="headClmn"> Daily Tasks </div>
-            <div class="clmns"> <asp:CheckBox ID="daily1" runat="server" /> </div>
-            <div class="clmns"> <asp:CheckBox ID="daily2" runat="server" /> </div>
-            <div class="clmns"> <asp:CheckBox ID="daily3" runat="server" /> </div>
+            <div class="clmns"> <asp:CheckBox ID="daily1" runat="server" AutoPostBack="true" OnCheckedChanged="daily1_CheckedChanged" /> </div>
+            <div class="clmns"> <asp:CheckBox ID="daily2" runat="server" AutoPostBack="true" OnCheckedChanged="daily2_CheckedChanged" /> </div>
+            <div class="clmns"> <asp:CheckBox ID="daily3" runat="server" AutoPostBack="true" OnCheckedChanged="daily3_CheckedChanged" /> </div>
         </div>
         <div class="space"></div>
         <div id="row2" class="rows"> 
             <div class="headClmn"> Weekly Tasks </div>
-            <div class="clmns"> <asp:CheckBox ID="weekly1" runat="server" /> </div>
-            <div class="clmns"> <asp:CheckBox ID="weekly2" runat="server" /> </div>
-            <div class="clmns"> <asp:CheckBox ID="weekly3" runat="server" /> </div>
+            <div class="clmns"> <asp:CheckBox ID="weekly1" runat="server" AutoPostBack="true" OnCheckedChanged="weekly1_CheckedChanged" /> </div>
+            <div class="clmns"> <asp:CheckBox ID="weekly2" runat="server" AutoPostBack="true" OnCheckedChanged="weekly2_CheckedChanged" /> </div>
+            <div class="clmns"> <asp:CheckBox ID="weekly3" runat="server" AutoPostBack="true" OnCheckedChanged="weekly3_CheckedChanged" /> </div>
         </div>
         <div class="space"></div>
         <div id="row3" class="rows"> 
-            <div class="clmns"> Chaos <asp:CheckBox ID="chaos1" runat="server" /> <asp:CheckBox ID="chaos2" runat="server" /> </div>
-            <div class="clmns"> Guardian Raid <asp:CheckBox ID="guardian1" runat="server" /> <asp:CheckBox ID="guardian2" runat="server" /> </div>
-            <div class="clmns"> Abbysal Dungeon <asp:CheckBox ID="abbysal" runat="server" /> </div>
+            <div class="clmns"> Chaos <asp:CheckBox ID="chaos1" runat="server" AutoPostBack="true" OnCheckedChanged="chaos1_CheckedChanged"  /> <asp:CheckBox ID="chaos2" runat="server" AutoPostBack="true" OnCheckedChanged="chaos2_CheckedChanged"  /> </div>
+            <div class="clmns"> Guardian Raid <asp:CheckBox ID="guardian1" runat="server" AutoPostBack="true" OnCheckedChanged="guardian1_CheckedChanged"  /> <asp:CheckBox ID="guardian2" runat="server" AutoPostBack="true" OnCheckedChanged="guardian2_CheckedChanged" /> </div>
+            <div class="clmns"> Abbysal Dungeon <asp:CheckBox ID="abbysal" runat="server" AutoPostBack="true" OnCheckedChanged="abbysal_CheckedChanged" /> </div>
         </div>
         <div class="space"></div>
         <div id="row4" class="rows"> 
-            <div class="clmns"> Cube <asp:CheckBox ID="cube" runat="server" /> </div>
-            <div class="clmns"> Boss Rush <asp:CheckBox ID="bossRush" runat="server" /> </div>
-            <div class="clmns"> Mystic Raid <asp:CheckBox ID="mysticRaid" runat="server" /> </div>
+            <div class="clmns"> Cube <asp:CheckBox ID="cube" runat="server" AutoPostBack="true" OnCheckedChanged="cube_CheckedChanged" /> </div>
+            <div class="clmns"> Boss Rush <asp:CheckBox ID="bossRush" runat="server" AutoPostBack="true" OnCheckedChanged="bossRush_CheckedChanged" /> </div>
+            <div class="clmns"> Mystic Raid <asp:CheckBox ID="mysticRaid" runat="server" AutoPostBack="true" OnCheckedChanged="mysticRaid_CheckedChanged"  /> </div>
         </div>
         <div class="space"></div>
         <div id="row5" class="rows"> 
-            <div class="clmns"> <img style="width:auto;height:80px" src="imgs/Icons/ship.png" alt="ship" /> <br> Merchant Ships  <asp:CheckBox ID="MerchantShips" runat="server" /> </div>
+            <div class="clmns"> <img style="width:auto;height:80px" src="imgs/Icons/ship.png" alt="ship" /> <br> Merchant Ships  <asp:CheckBox ID="MerchantShips" runat="server" AutoPostBack="true" OnCheckedChanged="MerchantShips_CheckedChanged" /> </div>
         </div>
         <div class="space"></div>
         <div id="row6" class="rows"> 
             <div class="headClmn"> Rapports </div>
-            <div class="clmns"> <asp:CheckBox ID="rapport1" runat="server" /> </div>
-            <div class="clmns"> <asp:CheckBox ID="rapport2" runat="server" /> </div>
-            <div class="clmns"> <asp:CheckBox ID="rapport3" runat="server" /> </div>
+            <div class="clmns"> <asp:CheckBox ID="rapport1" runat="server" AutoPostBack="true" OnCheckedChanged="rapport1_CheckedChanged" /> </div>
+            <div class="clmns"> <asp:CheckBox ID="rapport2" runat="server" AutoPostBack="true" OnCheckedChanged="rapport2_CheckedChanged" /> </div>
+            <div class="clmns"> <asp:CheckBox ID="rapport3" runat="server" AutoPostBack="true" OnCheckedChanged="rapport3_CheckedChanged" /> </div>
         </div>
     </div>
 <style>
+
+.hiddenBtn{
+    width: 0;
+    height: 0;
+    visibility: hidden;
+}
 
 .top-bar{
     font-size: 26px;
--- To-Do.aspx.cs
+++ To-Do.aspx.cs
@@ -9,24 +9,28 @@ using System.Web.UI.WebControls;
 public partial class Default2 : System.Web.UI.Page
 {
     public static string OwnerId { get;set;}
+    public static string charId { get; set; }
     public static string Nick { get; set; }
     public static string Class { get; set; }
+    public static string connectionString { get; set; }
+    public static SqlConnection con { get; set; }
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        string id = Request.QueryString["id"];
-        if(id == null)
+        charId = Request.QueryString["id"];
+        if(charId == null)
         {
             Response.Redirect("~/", true);
         }
 
-        Page Page = new Page();
-        string currentUserId = CurrentUserId.UserId(Page.User.Identity.Name);
+        Page _Page = new Page();
+        string currentUserId = CurrentUserId.UserId(_Page.User.Identity.Name);
 
-        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-        SqlConnection con = new SqlConnection(connectionString);
+        connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        con = new SqlConnection(connectionString);
+
         con.Open();
-        SqlCommand com = new SqlCommand(String.Format("select ownerId, nick, class from chars where id = '{0}'", id), con);
+        SqlCommand com = new SqlCommand(String.Format("select ownerId, nick, class from chars where id = '{0}'", charId), con);
         SqlDataReader rd = com.ExecuteReader();
         while (rd.Read())
         {
@@ -40,5 +44,137 @@ public partial class Default2 : System.Web.UI.Page
         {
             Response.Redirect("~/", true);
         }
+
+        if (!Page.IsPostBack)
+        {
+            con.Open();
+            SqlCommand com1 = new SqlCommand(String.Format("select daily1, daily2, daily3, weekly1, weekly2, weekly3, chaos1, chaos2, guardian1, guardian2, abbysal, cube, bossRush, mysticRaid, merchant, rapport1, rapport2, rapport3 from chars where id = '{0}'", charId), con);
+            SqlDataReader rd1 = com1.ExecuteReader();
+            while (rd1.Read())
+            {
+                daily1.Checked = Convert.ToBoolean(rd1[0]);
+                daily2.Checked = Convert.ToBoolean(rd1[1]);
+                daily3.Checked = Convert.ToBoolean(rd1[2]);
+                weekly1.Checked = Convert.ToBoolean(rd1[3]);
+                weekly2.Checked = Convert.ToBoolean(rd1[4]);
+                weekly3.Checked = Convert.ToBoolean(rd1[5]);
+                chaos1.Checked = Convert.ToBoolean(rd1[6]);
+                chaos2.Checked = Convert.ToBoolean(rd1[7]);
+                guardian1.Checked = Convert.ToBoolean(rd1[8]);
+                guardian2.Checked = Convert.ToBoolean(rd1[9]);
+                abbysal.Checked = Convert.ToBoolean(rd1[10]);
+                cube.Checked = Convert.ToBoolean(rd1[11]);
+                bossRush.Checked = Convert.ToBoolean(rd1[12]);
+                mysticRaid.Checked = Convert.ToBoolean(rd1[13]);
+                MerchantShips.Checked = Convert.ToBoolean(rd1[14]);
+                rapport1.Checked = Convert.ToBoolean(rd1[15]);
+                rapport2.Checked = Convert.ToBoolean(rd1[16]);
+                rapport3.Checked = Convert.ToBoolean(rd1[17]);
+            }
+            con.Close();
+        }
+    }
+    protected void BtnSave_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand com = new SqlCommand(String.Format("" +
+            "update chars set daily1 = '{0}', daily2 = '{1}', daily3 = '{2}', weekly1 = '{3}', weekly2 = '{4}', weekly3 = '{5}'," +
+           " chaos1 = '{6}', chaos2 = '{7}', guardian1 = '{8}', guardian2 = '{9}', abbysal = '{10}', cube = '{11}', bossRush = '{12}'," +
+           " mysticRaid = '{13}', merchant = '{14}', rapport1 = '{15}', rapport2 = '{16}', rapport3 = '{17}' where id = '{18}'",
+           daily1.Checked, daily2.Checked, daily3.Checked, weekly1.Checked, weekly2.Checked, weekly3.Checked, chaos1.Checked, chaos2.Checked,
+           guardian1.Checked, guardian2.Checked, abbysal.Checked, cube.Checked, bossRush.Checked, mysticRaid.Checked, MerchantShips.Checked,
+           rapport1.Checked, rapport2.Checked, rapport3.Checked, charId), con);
+        com.ExecuteNonQuery();
+        con.Close();
+    }
+
+    protected void daily1_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void daily2_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void daily3_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void weekly1_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void weekly2_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void weekly3_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void chaos1_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void chaos2_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void guardian1_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void guardian2_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void abbysal_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void cube_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void bossRush_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void mysticRaid_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void MerchantShips_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void rapport1_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void rapport2_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
+    }
+
+    protected void rapport3_CheckedChanged(object sender, EventArgs e)
+    {
+        BtnSave_Click(sender, e);
     }
 }
